5dde0674a7c5df7d1dee1be37790d318bb8c0d3e
dnagafonov <denisagafonovwork@gmail.com>
2019-11-21 13:13:05 +0300

    done lab8

---
+++ Lab8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace kpiyap
+{
+    public class Lab8
+    {
+        public static void task()
+        {
+            var fileText = File.ReadAllText("C:/progects/Rider/KPyaP/song.txt");
+            string[] arrayOfWords = fileText.Split(" ");
+            Console.WriteLine(arrayOfWords.Length);
+            int maxLenght = arrayOfWords[0].Length;
+            string longestWord = "";
+            for (int i = 1; i < arrayOfWords.Length; i++)
+            {
+                if (arrayOfWords[i].Length > maxLenght)
+                {
+                    maxLenght = arrayOfWords[i].Length;
+                    longestWord = arrayOfWords[i];
+                }
+            }
+            Console.WriteLine($"\nLongest word: {longestWord}\nWith length: {maxLenght}");
+        }
+    }
+}
