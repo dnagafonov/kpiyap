c86a48566cd7efe1f7b9307afad9a7f78ea5a8df
dnagafonov <denisagafonovwork@gmail.com>
2019-12-24 20:58:20 +0300

    some 17

---
+++ Lab17_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace kpiyap
+{
+    public class Lab17_1
+    {
+        private int[] arr;
+
+        public Lab17_1(){}
+        public Lab17_1(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        ~Lab17_1()
+        {
+            Console.WriteLine("Deleted");
+        }
+
+        public int this[int index]
+        {
+            get => arr[index];
+            set
+            {
+                if (index <= arr.Length && index > 0)
+                    arr[index] = value;
+                else
+                    throw new Exception("Incorrect value");
+            }
+        }
+    }
+}
