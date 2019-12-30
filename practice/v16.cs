9260a01c7e383764d1cb1efdb9ba7a73de5f116c
dnagafonov <denisagafonovwork@gmail.com>
2019-12-30 21:43:41 +0300

    4, 14, 16

---
+++ practice/v16.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+namespace kpiyap.practice
+{
+    public class Human
+    {
+        private int id;
+        private string name;
+        private bool married;
+        private bool hasHasWork;
+        private int salary;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name.Length < 0)
+                    throw new Exception("Error");
+                name = value;
+            }
+        }
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (id < 0)
+                    throw new Exception("Error");
+                id = value;
+            }
+        }
+        public bool Married
+        {
+            get => married;
+            set => married = value;
+        }
+        public bool HasWork
+        {
+            get => hasHasWork;
+            set => hasHasWork = value;
+        }
+        public int Salary
+        {
+            get => salary;
+            set
+            {
+                if (salary < 0)
+                    throw new Exception("Error");
+                salary = value;
+            }
+        }
+        public List<Human> Array { get; }
+
+
+        public Human()
+        {
+            Array = new List<Human>();
+        }
+
+        public Human(string name)
+        {
+            Name = name;
+        }
+        
+        public Human(int id, bool married)
+        {
+            Id = id;
+            Married = married;
+        }
+        
+        public Human(int id, string name, bool married)
+        {
+            Id = id;
+            Married = married;
+            Name = name;
+        }
+        
+        public Human(int id, string name, bool hasWork, int salary)
+        {
+            Salary = salary;
+            Name = name;
+            Id = id;
+            HasWork = hasWork;
+        }
+
+        public void sortBySalary()
+        {
+            Array.Sort((a,b) => a.Salary.CompareTo(b.Salary));
+            foreach (Human el in Array)
+                Console.WriteLine(el);
+        }
+        
+        public void sortByName()
+        {
+            Array.Sort((a,b) => String.Compare(a.Name, b.Name, StringComparison.Ordinal));
+            foreach (Human el in Array)
+                Console.WriteLine(el);
+        }
+        public void sortByHasWork()
+        {
+            Array.Sort((a,b) => a.HasWork.CompareTo(b.HasWork));
+            foreach (Human el in Array)
+                Console.WriteLine(el);
+        }
+        public void sortByMarried()
+        {
+            Array.Sort((a,b) => a.Married.CompareTo(b.Married));
+            foreach (Human el in Array)
+                Console.WriteLine(el);
+        }
+        public void sortById()
+        {
+            Array.Sort((a,b) => a.Id.CompareTo(b.Id));
+            foreach (Human el in Array)
+                Console.WriteLine(el);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Id: {0}, Married: {1}, Name: {2}, HasWork: {3}, Salary: {4}", Id, Married, Name, HasWork, Salary);
+        }
+    }
+}
---
+++ practice/v4.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+namespace kpiyap.practice
+{
+    public class Fruit
+    {
+        private int quantity;
+        private string name;
+        private int countOfSeeds;
+        private double weight;
+        private int price;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name.Length < 0)
+                    throw new Exception("Error");
+                name = value;
+            }
+        }
+        public int Quantity
+        {
+            get => quantity;
+            set
+            {
+                if (quantity < 0)
+                    throw new Exception("Error");
+                quantity = value;
+            }
+        }
+        public int CountOfSeeds
+        {
+            get => countOfSeeds;
+            set
+            {
+                if (countOfSeeds < 0)
+                    throw new Exception("Error");
+                countOfSeeds = value;
+            }
+        }
+        public double Weight
+        {
+            get => weight;
+            set
+            {
+                if (weight < 0)
+                    throw new Exception("Error");
+                weight = value;
+            }
+        }
+        public int Price
+        {
+            get => price;
+            set
+            {
+                if (price < 0)
+                    throw new Exception("Error");
+                price = value;
+            }
+        }
+        public List<Fruit> Array { get; }
+
+
+        public Fruit()
+        {
+            Array = new List<Fruit>();
+        }
+
+        public Fruit(string name)
+        {
+            Name = name;
+        }
+        
+        public Fruit(int quantity, int countOfSeeds)
+        {
+            Quantity = quantity;
+            CountOfSeeds = countOfSeeds;
+        }
+        
+        public Fruit(int quantity, string name, int countOfSeeds)
+        {
+            Quantity = quantity;
+            CountOfSeeds = countOfSeeds;
+            Name = name;
+        }
+        
+        public Fruit(int quantity, string name, int weight, int price)
+        {
+            Price = price;
+            Name = name;
+            Quantity = quantity;
+            Weight = weight;
+        }
+
+        public void sortByPrice()
+        {
+            Array.Sort((a,b) => a.Price.CompareTo(b.Price));
+            foreach (Fruit el in Array)
+                Console.WriteLine(el);
+        }
+        
+        public void sortByName()
+        {
+            Array.Sort((a,b) => String.Compare(a.Name, b.Name, StringComparison.Ordinal));
+            foreach (Fruit el in Array)
+                Console.WriteLine(el);
+        }
+        public void sortByQuantity()
+        {
+            Array.Sort((a,b) => a.Quantity.CompareTo(b.Quantity));
+            foreach (Fruit el in Array)
+                Console.WriteLine(el);
+        }
+        public void sortByWeight()
+        {
+            Array.Sort((a,b) => a.Weight.CompareTo(b.Weight));
+            foreach (Fruit el in Array)
+                Console.WriteLine(el);
+        }
+        public void sortByCountOfSeeds()
+        {
+            Array.Sort((a,b) => a.CountOfSeeds.CompareTo(b.CountOfSeeds));
+            foreach (Fruit el in Array)
+                Console.WriteLine(el);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Quantity: {0}, CountOfSeeds: {1}, Name: {2}, Weight: {3}, Price: {4}", Quantity, CountOfSeeds, Name, Weight, Price);
+        }
+    }
+}
