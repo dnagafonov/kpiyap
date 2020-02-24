5c1c7b385c8d83e99b5cca44db3a7fe145934acb
dnagafonov <denisagafonovwork@gmail.com>
2020-02-24 19:29:22 +0300

    feat: add solution of 22 lab

---
+++ lab22.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kpiyap
+{
+    public class Lab22
+    {
+        private static void writeFile(string text)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("Test.txt");
+                sw.WriteLine(text);
+                sw.Close();
+                Console.WriteLine("Done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finished");
+            }
+        }
+        private static void readFile()
+        {
+            try 
+            {
+                StreamReader sr = new StreamReader("Test.txt");
+                string line = sr.ReadLine();
+
+                while (line != null) 
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Executing finally block.");
+            }       
+        }
+
+        public static void read()
+        {
+            Thread thread = new Thread(new ThreadStart(readFile));
+            thread.Start();
+        }
+        
+        public static void write(string s)
+        {
+            writeFile(s);
+        }
+    }
+}
